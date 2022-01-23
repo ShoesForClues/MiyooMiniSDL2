718ac5a1290429825e34b8b6b4105451af400267
Shoelee <38192767+ShoesForClues@users.noreply.github.com>
2022-01-23 04:38:49 -0700

    Fix screen orientation

--- SDL2Demo/launch.sh
+++ SDL2Demo/launch.sh
@@ -4,5 +4,5 @@ cd $(dirname "$0")
 
 HOME=/mnt/SDCARD
 LD_LIBRARY_PATH=./lib
-export DFBARGS=module-dir=./lib/directfb-1.7-7
+export DFBARGS="module-dir=./lib/directfb-1.7-7,layer-rotate=180"
 ./testyuv &> log.txt
