9ec5b5a4dc5196bb7a2f2675035766d485246de9
Shoelee <38192767+ShoesForClues@users.noreply.github.com>
2022-01-24 20:59:12 -0700

    Add files via upload

--- SDL2Demo/launch.sh
+++ SDL2Demo/launch.sh
@@ -3,6 +3,6 @@ echo $0 $*
 cd $(dirname "$0")
 
 HOME=/mnt/SDCARD
-LD_LIBRARY_PATH=./lib
-export DFBARGS="module-dir=./lib/directfb-1.7-7,layer-rotate=180"
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
+export DFBARGS="module-dir=./lib/directfb-1.7-7,layer-rotate=180,layer-format=ABGR"
 ./testyuv &> log.txt
